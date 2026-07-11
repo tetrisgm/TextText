9de68e445213ab5c7c4792129bf8b21cc737da83
Shokunin <30949000+tetrisgm@users.noreply.github.com>
2026-07-10 18:37:11 -0700

    Release Write.app v0.18 (build 19)

--- src/generated/app-release.ts
+++ src/generated/app-release.ts
@@ -1,8 +1,8 @@
 export const generatedAppRelease = {
-  version: "0.17",
-  buildNumber: 18,
+  version: "0.18",
+  buildNumber: 19,
   appcastUrl:
-    "https://fkxlzavaphs5epsz.public.blob.vercel-storage.com/downloads/appcast-0.17.xml",
+    "https://fkxlzavaphs5epsz.public.blob.vercel-storage.com/downloads/appcast-0.18.xml",
   zipUrl:
-    "https://fkxlzavaphs5epsz.public.blob.vercel-storage.com/downloads/Write-0.17.zip",
+    "https://fkxlzavaphs5epsz.public.blob.vercel-storage.com/downloads/Write-0.18.zip",
 } as const;
