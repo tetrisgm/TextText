f05fbfc9df6aaa9860fc6a8fea0d000a969e6c91
Shokunin <30949000+tetrisgm@users.noreply.github.com>
2026-07-17 22:22:46 -0700

    Release Write 0.94

--- src/generated/app-release.ts
+++ src/generated/app-release.ts
@@ -1,8 +1,8 @@
 export const generatedAppRelease = {
-  version: "0.93",
-  buildNumber: 98,
+  version: "0.94",
+  buildNumber: 100,
   appcastUrl:
-    "https://fkxlzavaphs5epsz.public.blob.vercel-storage.com/downloads/appcast-0.93.xml",
+    "https://fkxlzavaphs5epsz.public.blob.vercel-storage.com/downloads/appcast-0.94.xml",
   zipUrl:
-    "https://fkxlzavaphs5epsz.public.blob.vercel-storage.com/downloads/Write-0.93.zip",
+    "https://fkxlzavaphs5epsz.public.blob.vercel-storage.com/downloads/Write-0.94.zip",
 } as const;
