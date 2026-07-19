aea7650f7b4693e83e86a680a98ca1f49aecb136
Shokunin <30949000+tetrisgm@users.noreply.github.com>
2026-07-18 21:08:13 -0700

    Record Write 0.103 release

--- src/generated/app-release.ts
+++ src/generated/app-release.ts
@@ -1,8 +1,8 @@
 export const generatedAppRelease = {
-  version: "0.102",
-  buildNumber: 108,
+  version: "0.103",
+  buildNumber: 109,
   appcastUrl:
-    "https://fkxlzavaphs5epsz.public.blob.vercel-storage.com/downloads/appcast-0.102.xml",
+    "https://fkxlzavaphs5epsz.public.blob.vercel-storage.com/downloads/appcast-0.103.xml",
   zipUrl:
-    "https://fkxlzavaphs5epsz.public.blob.vercel-storage.com/downloads/Write-0.102.zip",
+    "https://fkxlzavaphs5epsz.public.blob.vercel-storage.com/downloads/Write-0.103.zip",
 } as const;
