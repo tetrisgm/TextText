b96e36dfa2faa5b108d6469b9131fa9c9a091d79
Shokunin <30949000+tetrisgm@users.noreply.github.com>
2026-07-27 10:28:40 -0700

    Record Texttext 0.127 release

--- src/generated/app-release.ts
+++ src/generated/app-release.ts
@@ -1,8 +1,8 @@
 export const generatedAppRelease = {
-  version: "0.126",
-  buildNumber: 132,
+  version: "0.127",
+  buildNumber: 133,
   appcastUrl:
-    "https://fkxlzavaphs5epsz.public.blob.vercel-storage.com/downloads/appcast-0.126.xml",
+    "https://fkxlzavaphs5epsz.public.blob.vercel-storage.com/downloads/appcast-0.127.xml",
   zipUrl:
-    "https://fkxlzavaphs5epsz.public.blob.vercel-storage.com/downloads/Texttext-0.126.zip",
+    "https://fkxlzavaphs5epsz.public.blob.vercel-storage.com/downloads/Texttext-0.127.zip",
 } as const;
