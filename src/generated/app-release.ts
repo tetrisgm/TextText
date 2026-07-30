bfe024890afcd3cd563792870cc9770522d171f2
Shokunin <30949000+tetrisgm@users.noreply.github.com>
2026-07-29 17:07:31 -0700

    Release Texttext 0.159

--- src/generated/app-release.ts
+++ src/generated/app-release.ts
@@ -1,8 +1,8 @@
 export const generatedAppRelease = {
-  version: "0.158",
-  buildNumber: 164,
+  version: "0.159",
+  buildNumber: 165,
   appcastUrl:
-    "https://fkxlzavaphs5epsz.public.blob.vercel-storage.com/downloads/appcast-0.158.xml",
+    "https://fkxlzavaphs5epsz.public.blob.vercel-storage.com/downloads/appcast-0.159.xml",
   zipUrl:
-    "https://fkxlzavaphs5epsz.public.blob.vercel-storage.com/downloads/Texttext-0.158.zip",
+    "https://fkxlzavaphs5epsz.public.blob.vercel-storage.com/downloads/Texttext-0.159.zip",
 } as const;
